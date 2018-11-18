babe6d39c00dba66b235d2e53b3beefb6b4c7aef
zhacaigeng <1449616191@qq.com>
2018-11-18 21:37:53 +0800

    commit

---
+++ Vue父子组件传递总结/013ParentToChild.vue
@@ -0,0 +1,33 @@
+// 父组件
+<template>
+    <div>
+        <child @sendToChild = 'SendTo'></child>
+    </div>
+</template>
+
+<script>
+    export default {
+        el: "",
+        data: function  () {
+            return {
+                SendTo: '张三丰'
+            }
+        }
+
+
+    }
+</script>
+
+// 子组件
+
+<script>
+    export default {
+        el:"",
+        data: function  () {
+            return {
+                *** : this.sendToChild
+            }
+        },
+        pros:["sendToChild"]
+    }
+</script>
---
+++ Vue父子组件传递总结/014ChildToParent.vue
@@ -0,0 +1,29 @@
+
+// 父组件
+<template>
+    <div>
+        <child @SendToParent="zhangsanf"></child>
+    </div>
+</template>
+
+
+
+    methods:{
+        zhangsanf:  function (arguments) {
+            conosle.log(argurment)
+            //  "i am son"
+        }
+    }
+
+
+
+
+
+// 子组件
+<script>
+    this.$emit('SendToParent','i am son')
+</script>
+
+
+
+
