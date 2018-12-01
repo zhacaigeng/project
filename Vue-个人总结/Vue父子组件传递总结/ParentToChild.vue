2128bc83672149e44f2aff1ee95554c96fad1a3d
zhacaigeng <1449616191@qq.com>
2018-12-01 16:15:53 +0800

    add

---
+++ Vue-个人总结/Vue父子组件传递总结/ParentToChild.vue
@@ -0,0 +1,29 @@
+// 父组件
+<template>
+    <div>
+        <child @sendToChild = 'SendTo'></child>
+        //       儿子           父亲
+    </div>
+</template>
+
+<script>
+    export default {
+        el: "",
+        data: function  () {
+            return {
+                SendTo: 'i am father'
+            }
+        }
+    }
+</script>
+
+// 子组件
+<script>
+        el:"",
+        props:["sendToChild"]
+        data: function  () {
+            return {
+                *** : this.sendToChild  //'i am father'
+            }
+        },    
+</script>
---
+++ Vue-个人总结/Vue起步个人见解/008.js
@@ -0,0 +1,5 @@
+
+new Vue({
+    el:"#app",
+    template:"<div>我只一个兵</div>"
+})
